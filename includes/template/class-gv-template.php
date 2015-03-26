b9a6ea845d38046020cbe71c79c438a88cbbf578
Zack Katz <zack@katzwebservices.com>
2015-03-26 10:43:15 -0600

    Getting confused

--- includes/template/class-gv-template.php
+++ includes/template/class-gv-template.php
@@ -2,7 +2,70 @@
 
 /**
  * Handle outputting the View
+ * @todo HELP!
  */
 class GV_Template( GV_View_Settings $GV_View_Settings ) {
+
+	/**
+	 * @var GV_Template_Zone[]
+	 */
+	private $zones = array();
+
+	function __construct() {
+
+	}
+
+	function render() {
+		foreach( $this->zones as $zone ) {
+			$zone->render();
+		}
+	}
+
+}
+
+/**
+ * Class GV_Template_Zone
+ * @todo HELP!
+ */
+class GV_Template_Zone {
+
+	/**
+	 * @var GV_Template_Field[]
+	 */
+	var $fields;
+
+	public function render() {
+
+		foreach( $fields as $field ) {
+
+			$field->render();
+
+		}
+	}
+
+}
+
+/**
+ * Class GV_Template_Field
+ * @todo HELP!
+ */
+class GV_Template_Field {
+
+	/**
+	 * @var GF_Field
+	 */
+	var $field;
+
+	public function render() {
+
+	}
+
+	private function get_field_label( $force_frontend_label, $value ) {
+
+	}
+
+	private function get_field_content() {
+		$field->get_field_content();
+	}
 
 }
